70e2562420e503f26cf0ef5621f5477ba3080355
Yahia <yahiaabdelbar@gmail.com>
2020-08-11 16:34:59 +0200

    Add Java Servlet to fetch country codes

---
+++ trending_data/src/main/java/com/google/sps/servlets/CountryCodeServlet.java
@@ -0,0 +1,31 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import com.google.sps.data.Country;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/countries")
+public class CountryCodeServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, FileNotFoundException {
+    response.setContentType("application/json;");
+    ArrayList<Country> countries =new ArrayList<Country>();
+    String csvFile = "WEB-INF/countries_codes.csv";
+    BufferedReader br = new BufferedReader(new FileReader(csvFile));
+    // discard headers
+    br.readLine();
+    while(br.ready()) {
+        countries.add(new Country(br.readLine()));
+    }
+    response.getWriter().println(new Gson().toJson(countries));
+  }
+}
---
+++ trending_data/src/main/webapp/script.js
@@ -0,0 +1,4 @@
+
+function readCsv() {
+    const fr = new File 
+}
