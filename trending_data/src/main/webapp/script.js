014f8c430ac1d48c6cc783d9a8b7561473a4c592
Ioana Lazar <ioanalazar459@gmail.com>
2020-08-11 09:31:18 +0000

    Display hardcoded map and do linter setup

---
+++ trending_data/src/main/webapp/script.js
@@ -0,0 +1,13 @@
+let map;
+
+/* eslint-disable no-unused-vars */
+/**
+
+ * Initialise map
+ */
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8,
+  });
+} /* eslint-enable no-unused-vars */
