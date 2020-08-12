65912f680d43511a905f0d834e8ce11c8e62f4f6
Yahia <yahiaabdelbar@gmail.com>
2020-08-12 15:41:05 +0200

    Move Parsing to a seperate method

--- trending_data/src/main/java/com/google/sps/data/Country.java
+++ trending_data/src/main/java/com/google/sps/data/Country.java
@@ -7,33 +7,39 @@ public class Country {
   String alpha2Code = "";
   double lat;
   double lng;
-
   // Given a String in this format:
   // "Country","Alpha-2 code","Alpha-3 code","Numeric code","Latitude (average)","Longitude
   // (average)"
   // , creates the Country object which contains country name , alpha-2 code and the  lat,lng
-  public Country(String s) {
+  public Country(String countryInfo) {
+    
+    // will fail if country name has a (") inside it
+    ArrayList<String> fields = parse(countryInfo);
+    
+    this.name = fields.get(0);
+    this.alpha2Code = fields.get(1);
+    this.lat = Double.parseDouble(fields.get(4));
+    this.lng = Double.parseDouble(fields.get(5));
+    
+  }
+
+  // gets the info out of the string in the above format
+  private static ArrayList<String> parse(String countryInfo) {
+    ArrayList<String> fields = new ArrayList<String>();
     // true if the index (i) is inside a pair of ("")
-    // will fail if string has a (") inside a pair of ("")
-    ArrayList<String> fields = new ArrayList();
     boolean insideQuote = false;
     StringBuilder token = new StringBuilder();
-    for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i) == '"') {
+    for (int i = 0; i < countryInfo.length(); i++) {
+      if (countryInfo.charAt(i) == '"') {
         if (insideQuote) {
           fields.add(token.toString());
           token = new StringBuilder();
         }
         insideQuote = !insideQuote;
       } else if (insideQuote) {
-        token.append(s.charAt(i));
+        token.append(countryInfo.charAt(i));
       }
     }
-    if (fields.size() == 6) {
-      this.name = fields.get(0);
-      this.alpha2Code = fields.get(1);
-      this.lat = Double.parseDouble(fields.get(4));
-      this.lng = Double.parseDouble(fields.get(5));
-    }
+    return fields;
   }
 }
--- trending_data/src/main/java/com/google/sps/servlets/CountryCodeServlet.java
+++ trending_data/src/main/java/com/google/sps/servlets/CountryCodeServlet.java
@@ -4,8 +4,8 @@ import com.google.gson.Gson;
 import com.google.sps.data.Country;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,8 +20,12 @@ public class CountryCodeServlet extends HttpServlet {
       throws IOException, FileNotFoundException {
     response.setContentType("application/json;");
     ArrayList<Country> countries = new ArrayList<Country>();
-    String csvFile = "WEB-INF/countries_codes.csv";
-    BufferedReader br = new BufferedReader(new FileReader(csvFile));
+    // String csvFile = "WEB-INF/countries_codes.csv";
+
+    BufferedReader br =
+        new BufferedReader(
+            new InputStreamReader(
+                CountryCodeServlet.class.getResourceAsStream("/countries_codes.csv")));
     // discard headers
     br.readLine();
     while (br.ready()) {
