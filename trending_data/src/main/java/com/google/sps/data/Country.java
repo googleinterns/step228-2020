f9ca7faec6315651d85d3c2ebcccc423b1a85f62
Yahia <yahiaabdelbar@gmail.com>
2020-08-12 16:12:17 +0200

    Lint

--- trending_data/src/main/java/com/google/sps/data/Country.java
+++ trending_data/src/main/java/com/google/sps/data/Country.java
@@ -12,22 +12,21 @@ public class Country {
   // (average)"
   // , creates the Country object which contains country name , alpha-2 code and the  lat,lng
   public Country(String countryInfo) {
-    
+
     // will fail if country name has a (") inside it
     ArrayList<String> fields = parse(countryInfo);
-    
+
     this.name = fields.get(0);
     this.alpha2Code = fields.get(1);
     this.lat = Double.parseDouble(fields.get(4));
     this.lng = Double.parseDouble(fields.get(5));
-    
   }
 
   // gets the info out of the string in the above format
   private static ArrayList<String> parse(String countryInfo) {
     ArrayList<String> fields = new ArrayList<String>();
     // true if the index (i) is inside a pair of ("")
-   boolean insideQuote = false;
+    boolean insideQuote = false;
     StringBuilder token = new StringBuilder();
     for (int i = 0; i < countryInfo.length(); i++) {
       if (countryInfo.charAt(i) == '"') {
