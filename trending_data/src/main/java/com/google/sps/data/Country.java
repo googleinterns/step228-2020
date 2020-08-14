5faaeb0948194700d3d6624addde772c986c6e19
Yahia <yahiaabdelbar@gmail.com>
2020-08-14 12:02:46 +0200

    Improve code readablity

--- trending_data/src/main/java/com/google/sps/data/Country.java
+++ trending_data/src/main/java/com/google/sps/data/Country.java
@@ -3,26 +3,34 @@ package com.google.sps.data;
 import java.util.ArrayList;
 
 public class Country {
-  String name = "";
-  public String alpha2Code = "";
-  double lat;
-  double lng;
+  private String name = "";
+  private String alpha2Code = "";
+  private double lat;
+  private double lng;
+  private static final int COUNTRY_NAME_INDEX = 0;
+  private static final int COUNTRY_ALPHA_2_CODE_INDEX = 1;
+  private static final int COUNTRY_ALPHA_3_CODE_INDEX = 2;
+  private static final int COUNTRY_NUMERIC_CODE_INDEX = 3;
+  private static final int COUNTRY_LATITUDE_INDEX = 4;
+  private static final int COUNTRY_LONGITUDE_INDEX = 5;
   // Given a String in this format:
   // "Country","Alpha-2 code","Alpha-3 code","Numeric code","Latitude (average)","Longitude
   // (average)"
   // , creates the Country object which contains country name , alpha-2 code and the  lat,lng
+  // Codes are (ISO 3166-1) codes
   public Country(String countryInfo) {
 
     // will fail if country name has a (") inside it
     ArrayList<String> fields = parse(countryInfo);
 
-    this.name = fields.get(0);
-    this.alpha2Code = fields.get(1);
-    this.lat = Double.parseDouble(fields.get(4));
-    this.lng = Double.parseDouble(fields.get(5));
+    this.name = fields.get(COUNTRY_NAME_INDEX);
+    this.alpha2Code = fields.get(COUNTRY_ALPHA_2_CODE_INDEX);
+    this.lat = Double.parseDouble(fields.get(COUNTRY_LATITUDE_INDEX));
+    this.lng = Double.parseDouble(fields.get(COUNTRY_LONGITUDE_INDEX));
   }
 
   // gets the info out of the string in the above format
+  // splitting on (,) will not work because some country names have (,) 
   private static ArrayList<String> parse(String countryInfo) {
     ArrayList<String> fields = new ArrayList<String>();
     // true if the index (i) is inside a pair of ("")
