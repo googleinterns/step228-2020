b56bf62f9d8ec952aeac8eee3b47e6f76521c13c
Ioana Lazar <ioanalazar459@gmail.com>
2020-08-20 10:21:58 +0000

    Make sure that the method which parses country information does not accept an empty string

--- trending_data/src/main/java/com/google/sps/data/Country.java
+++ trending_data/src/main/java/com/google/sps/data/Country.java
@@ -9,6 +9,7 @@ public class Country {
   private static final int COUNTRY_ALPHA_2_CODE_INDEX = 1;
   private static final int COUNTRY_LATITUDE_INDEX = 4;
   private static final int COUNTRY_LONGITUDE_INDEX = 5;
+  private static final int NUMBER_OF_FIELDS = 6;
   // Given a String in this format: Country\tAlpha-2 code\tAlpha-3 code\t
   // \tNumeric code\tLatitude(average)\tLongitude(average)
   // creates the Country object which contains
@@ -23,6 +24,11 @@ public class Country {
   }
 
   public String[] parse(String countryInfo) {
+    /** If countryInfo is empty, throw exception */
+    if (countryInfo.isEmpty()) {
+      throw new IllegalArgumentException("Country info should not be empty");
+    }
+
     return countryInfo.split("\t");
   }
 }
