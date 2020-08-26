67c95a1dea3b09de0853b75f1188f6e76cce7416
Anya Sedova <anutkased05@gmail.com>
2020-08-26 08:43:33 +0000

    fix codestyle

--- trending_data/src/main/webapp/script.js
+++ trending_data/src/main/webapp/script.js
@@ -112,11 +112,11 @@ function displayPosts(marker) {
       const ytErr = '<h2>Region not supported by YouTube</h2>';
       windowsHandler.openwindow(marker, ytErr);
     } else { // if country is supported, fetch data
-      fetch('/ListYTLinks?country-code=' + marker.countryCode).then((response) 
-        => response.json()).then((videos) => {
-        const vidNode = getVideosNode(videos);
-        windowsHandler.openwindow(marker, vidNode);
-      });
+      fetch('/ListYTLinks?country-code=' + marker.countryCode).
+          then((response) => response.json()).then((videos) => {
+            const vidNode = getVideosNode(videos);
+            windowsHandler.openwindow(marker, vidNode);
+          });
     }
   }
 }
