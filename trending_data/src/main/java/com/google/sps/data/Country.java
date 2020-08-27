4580b50f475aedf6d50dcb52218bb70c331f6dc4
Ioana Lazar <ioanalazar459@gmail.com>
2020-08-27 09:24:37 +0000

    Change Country.java class to have a woeid field

--- trending_data/src/main/java/com/google/sps/data/Country.java
+++ trending_data/src/main/java/com/google/sps/data/Country.java
@@ -9,7 +9,8 @@ public class Country {
   private static final int COUNTRY_ALPHA_2_CODE_INDEX = 1;
   private static final int COUNTRY_LATITUDE_INDEX = 4;
   private static final int COUNTRY_LONGITUDE_INDEX = 5;
-  private static final int NUMBER_OF_FIELDS = 6;
+  private static final int WOEID_INDEX = 6; /** Where On Earth ID*/
+  private static final int NUMBER_OF_FIELDS = 7;
   // Given a String in this format: Country\tAlpha-2 code\tAlpha-3 code\t
   // \tNumeric code\tLatitude(average)\tLongitude(average)
   // creates the Country object which contains
@@ -21,6 +22,7 @@ public class Country {
     this.alpha2Code = fields[COUNTRY_ALPHA_2_CODE_INDEX];
     this.lat = Double.parseDouble(fields[COUNTRY_LATITUDE_INDEX]);
     this.lng = Double.parseDouble(fields[COUNTRY_LONGITUDE_INDEX]);
+    this.woeid = Long.parseLong(fields[WOEID_INDEX]);
   }
 
   public static String[] parse(String countryInfo) {
