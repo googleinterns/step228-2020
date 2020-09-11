548cfb5129dd5064867886d0e79ae66ab2722769
YahiaSherif28 <yahiaabdelbar@gmail.com>
2020-09-11 12:31:01 +0000

    Add a button that redirects the user to twitter in the tweets side panel

--- trending_data/src/main/webapp/handle_twitter.js
+++ trending_data/src/main/webapp/handle_twitter.js
@@ -52,33 +52,36 @@ function createTrendElement(trend) {
   tweetVolume.className = 'tweet-volume';
   tweetVolume.innerText = 'Tweets: ' + trend.tweetVolume;
 
-  const trendEl = document.createElement('a');
-  trendEl.href = trend.url;
-  trendEl.target = '_blank'; /** open link in new tab */
+  const trendEl = document.createElement('p');
   trendEl.className = 'trend-element';
   trendEl.appendChild(trendName);
+
   if (trend.tweetVolume != -1) {
-    /** diplay tweet volume if available */
+    // diplay tweet volume if available
     trendEl.appendChild(tweetVolume);
   }
   trendEl.addEventListener('click', () => {
-    showTweets(trend.query);
+    showTweets(trend.query, trend.url);
   });
   return trendEl;
 }
 
 /**
- * @param {string} query
  * Clears the tweets container then
  * Adds the top tweets returned by the query to the tweets container.
+ * @param {string} query
+ * @param {string} url The url that searches the query in twitter.
  */
-function showTweets(query) {
+function showTweets(query, url) {
   const tweetContainer = document.getElementById('tweets-container');
   const closeButton = createTwitterCloseButton();
+  const openInTwitterLink = createOpenInTwitterLink(url);
   tweetContainer.innerHTML = '';
   tweetContainer.style.width = '500px';
   tweetContainer.appendChild(closeButton);
+  tweetContainer.append(openInTwitterLink);
 
+  // fetch the IDs of the top tweets then put them in the tweetContainer
   fetch('/get-tweets?query='+query).then((response) => response.json())
       .then((tweetIDs) => {
         for (const id of tweetIDs) {
@@ -86,13 +89,14 @@ function showTweets(query) {
         }
       } );
 };
+
 /**
  * returns a button that closes the tweets container
  * @return {HTMLButtonElement}
  */
 function createTwitterCloseButton() {
   const closeButton = document.createElement('button');
-  closeButton.className = 'btn btn-default twitter-close';
+  closeButton.className = 'btn btn-default';
   closeButton.textContent = 'Close';
   closeButton.addEventListener('click', () => {
     const tweetContainer = document.getElementById('tweets-container');
@@ -100,4 +104,17 @@ function createTwitterCloseButton() {
     tweetContainer.style.width = '0px';
   });
   return closeButton;
+}
+/**
+ * returns a button that redirects the user to the url in a new tab
+ * @param {string} url The url that searches the hashtag/topic
+ * @return {HTMLElement}
+ */
+function createOpenInTwitterLink(url) {
+  const link = document.createElement('a');
+  link.href = url;
+  link.target = '_blank';// open link in new tab
+  link.id = 'open-in-twitter';
+  link.innerText = 'Open in twitter';
+  return link;
 }
